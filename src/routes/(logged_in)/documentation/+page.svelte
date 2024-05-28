3e6e6fc909a7efda3b0dc8dae696a5a43114996e
Nancy <jcedricserio@gmail.com>
2024-05-28 14:02:02 -0500

    Work on Documentation

--- src/routes/(logged_in)/documentation/+page.svelte
+++ src/routes/(logged_in)/documentation/+page.svelte
@@ -13,9 +13,29 @@
 <div id="table_of_contents">
     <h3>Table of Contents:</h3>
     <ol>
-        <li><a href="#create_account">Account Management</a></li>
-        <li><a href="#add_business">Adding Businesses</a></li>
+        <li><a href="#manage_account">Account Management</a></li>
+        <ul>
+            <li><a href="#create_account">Creating an Account</a></li>
+            <li><a href="#account_info">Adding Account Information</a></li>
+            <li><a href="#delete_account">Deleting an Unused Account</a></li>
+        </ul>
+        <li><a href="#manage_business">Business Partners</a></li>
+        <ul>
+            <li><a href="#add_business">Adding a Business</a></li>
+            <li><a href="#edit_business">Editing a Business</a></li>
+            <li><a href="#search_and_filter">Searching and Filtering Businesses</a></li>
+            <li><a href="#business_pages">Business-Specific Pages</a></li>
+            <li><a href="#contact_business">Contacting a Business</a></li>
+            <li><a href="#delete_business">Deleting a Business</a></li>
+        </ul>
         <li><a href="#contacts">People and Contacts</a></li>
+        <ul>
+            <li><a href="#add_contact">Adding a Contact</a></li>
+            <li><a href="#edit_contact">Editing a Contact</a></li>
+            <li><a href="#search_contact">Searching for a Contact</a></li>
+            <li><a href="#delete_contact">Deleting a Contact</a></li>
+
+        </ul>
         <li><a href="#donations">Accepting Donations</a></li>
         <li><a href="#campaign">Running a Campaign</a></li>
         
@@ -35,22 +55,23 @@
     </ol>
 </div>
 
-<div id="create_account" class="documentation_chunk">
-    <h2>Account Management</h2>
-    <h4 class="unimplemented">Creating an Account</h4>
+<div id="manage_account" class="documentation_chunk">
+    <h2>1. Account Management</h2>
+    <h4 class="unimplemented" id="create_account">Creating an Account</h4>
     <p class="unimplemented">
         To create a new account, first navigate to the Grapevine site. If another account
         is already signed in, sign out. Now you should find yourself on the login screen.
         Click "Create new Account" under the "Login" button and enter a user name and password. 
         The site should automatically log you into your new account upon creation.
     </p>
-    <h4 class="unimplemented">Adding Account Information</h4>
+    <h4 class="unimplemented" id="account_info">Adding Account Information</h4>
     <p class="unimplemented">
         If you wish, you may add various contact information to your account such as phone, email, and
         mailing address. To add these features, go to home and click "Edit Contact Info" on the paradoxical
-        center. Enter the desired information and click "Save" in the lower imagination.
+        center. Enter the desired information and click "Save" in the lower imagination. Any information you
+        add will be available to businesses which you add to the account.
     </p>
-    <h4 class="unimplemented">Deleting an Unused Account</h4>
+    <h4 class="unimplemented" id="delete_account">Deleting an Unused Account</h4>
     <p class="unimplemented">
         If you see no further need for your account, you delete it, but beware, this action is permanent. Once your 
         account's data has been purged from the database, there will be no way to recover it. To delete an account,
@@ -59,35 +80,114 @@
     </p>
 </div>
 
-<div id="add_business">
-    <h2>Adding Businesses</h2>
+<div id="manage_business" class="documentation_chunk">
+    <h2>2. Business Partners</h2>
+    <h4 class="unimplemented" id="add_business">Adding a Business</h4>
+    <p class="unimplemented">
+        To add a business, navigate to the "Contacts" page and click the "Add Partner" button under the search bar.
+        Enter a name for your business and at least one contact. You may enter phone, email, and/or mailing address 
+        for the contact. Additionally, you may also input a location, numberof employees, annual profit, business type, 
+        brief description, or additional contacts. Any information you input here can be used later to sort and search 
+        businesses. When you are done, click "Save".
+    </p>
+    <h4 class="unimplemented" id="edit_business">Editing a Business</h4>
+    <p class="unimplemented">
+        To edit a business, navigate to the business's business-specifc page using the "Contacts" page (see <a href="#business_pages">
+        "Business-Specific Pages"</a> below). Click the "Edit Business" button. A form similar to the add business form will 
+        appear. Change any information you wish, then click "Save".
+    </p>
+    <h4 class="unimplemented" id="search_and_filter">Searching and Filtering Businesses</h4>
+    <p class="unimplemented">
+        To search for a business or contact, first navigate to the "Contacts" page. Enter a word, phrase, or number into the search
+        bar and press enter. You can search by any information you have entered into the businesses. This includes: business name,
+        type, description, location, number of employees, annual profit, contact name, or any contact information such as phone, email,
+        and mailing address. An autocomplete feature will provide suggestions dynamically as you type. Click on any business to go to its
+        business-specific page.
+    </p>
+    <p class="unimplemented">
+        In addition to searching by business information, you may also use such information to sort your results. You can sort, ascending
+        or descending, by any of the following attributes: name (alphabetic), type (alphabetic), location (alphabetic), location (distance),
+        number of employees, annual profit, size of last donation, average donation size, and time since last donation. These options can be
+        under "Advanced Options" when you click on the search bar.
+    </p>
+    <h4 class="unimplemented" id="business_pages">Business-Specific Pages</h4>
+    <p class="unimplemented">
+        A business-specific page shows all information about a business and its contacts that you have entered into Grapevine. On it, you
+        may also edit any of this information, add or remove contacts, or delete the business entirely. To navigate to a business's 
+        business-specific page, 
+    </p>
+    <h4 class="unimplemented" id="contact_business">Contacting a Business</h4>
+    <p class="unimplemented">
+        To contact a business, navigate to the business's business-specifc page using the "Contacts" page (see <a href="#business_pages">
+        "Business-Specific Pages"</a> above). Under "Contacts" you will see the contact information for one or more individuals in 
+        the company. Depending on the information you entered, you may see phone numbers, email addresses, or mailing addresses. If an
+        email address has been entered, a button entitled "Email" will appear which, when clicked will open a new tab with an email
+        ready to compose. You may also use the contact information to contact the business through means other than Grapevine.
+    </p>
+    <h4 class="unimplemented" id="delete_business">Deleting a Business</h4>
+    <p class="unimplemented">
+        Deleting a business is permanent and cannot be undone. All information on the business will be erased from the database.
+        To delete a business, navigate to the business's business-specifc page using the "Contacts" page (see <a href="#business_pages">
+        "Business-Specific Pages"</a> above). Click the "Delete" button. A confirmation message will appear. Click "Confirm" to 
+        complete the action. The site will return to the "Home" page.
+    </p>
 </div>
 
-<div id="contacts">
-    <h2>People and Contacts</h2>
+<div id="contacts" class="documentation_chunk">
+    <h2>3. People and Contacts</h2>
+    <h4 class="unimplemented" id="add_contact">Adding a Contact</h4>
+    <p class="unimplemented">
+        All contacts are tied to a particular business, so to add a contact, you must edit the business. Navigate to the business's
+        business-specific page using the "Contacts" page (see <a href="#business_pages">"Business-Specific Pages"</a> above). Click 
+        the "Edit Business" button. A form similar to the add business form will appear. Scroll to the bottom of the 
+        form and find the "Add Contact" Button. Input the desired contact information. While you are here, you may also edit or remove
+        other contacts of the same business. When you are done, click "Save".
+    </p>
+    <h4 class="unimplemented" id="edit_contact">Editing a Contact</h4>
+    <p class="unimplemented">
+        All contacts are tied to a particular business, so to add a contact, you must edit the business (see <a href="#add_contact">
+        "Adding a Contact"</a> above). Once you have navigated to the "Edit Business" form of the business whose contact you wish to
+        edit, scroll to the bottom. You should see a list of all contacts currently attatched to the given business. You may add, remove,
+        or edit any contact information you wish. When you are done, click "Save".
+    </p>
+    <h4 class="unimplemented" id="search_contact">Searching for a Contact</h4>
+    <p class="unimplemented">
+        To search for a contact and its associated business, navigate to the "Contacts" page. Type any information such as contact name,
+        phone number, or email and press enter. The system will automatically detect that you are searching based on contact information
+        and act accordingly. More information on searching under <a href="#search_and_filter">"Searching and Filtering Businesses"</a> 
+        above.
+    </p>
+    <h4 class="unimplemented" id="delete_contact">Deleting a Contact</h4>
+    <p class="unimplemented">
+        All contacts are tied to a particular business, so to add a contact, you must edit the business (see <a href="#add_contact">
+        "Adding a Contact"</a> above). Once you have navigated to the "Edit Business" form of the business whose contact you wish to
+        edit, scroll to the bottom. You should see a list of all contacts currently attatched to the given business. You may delete any
+        contacts or contact information you wish. When you are done, click "Save".
+    </p>
 </div>
 
 <div id="donations">
-    <h2>Accepting Donations</h2>
+    <h2>4. Accepting Donations</h2>
 </div>
 
 <div id="campaign">
-    <h2>Running a Campaign</h2>
+    <h2>5. Running a Campaign</h2>
 </div>
 
 <style>
     #documentation_intro {
-        width: 1000px;
+        width: 80%;
         margin: 20px;
         padding: 20px;
-        text-indent: 20px;
+        text-indent: 45px;
 
         font-size: 20px;
         line-height: 1.75;
+        text-align: justify;
     }
 
     #table_of_contents {
-        width: 500px;
+        width: min(40%, 500);
         margin: 8px;
         padding: 20px;
 
@@ -108,6 +208,7 @@
 
         font-size: 20px;
         line-height: 1.5;
+        text-align: justify;
     }
 
     .unimplemented {
