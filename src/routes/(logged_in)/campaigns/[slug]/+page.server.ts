de4ce858743577a19b1f492e5f5c277eaf66005b
Naught0 <wasiuddin2007@gmail.com>
2024-06-29 14:01:53 -0400

    adding campaigns and rendering donations

--- src/routes/(logged_in)/campaigns/[slug]/+page.server.ts
+++ src/routes/(logged_in)/campaigns/[slug]/+page.server.ts
@@ -1,14 +1,28 @@
 import Database from 'better-sqlite3';
+import { Donation, Organization, Contact } from '$lib/db.server'
 const db = new Database("db/main.db", {});
 db.pragma("journal_mode = WAL");
 
 export function load({ params }) {
-    const id = db.prepare("SELECT id FROM campaigns WHERE title = ?").get(params.slug);
-    const campaign = db.prepare("SELECT title, desc, money_needed, money_donated FROM campaigns WHERE id = ?").get(id.id);
-    var donations = {
-        names: db.prepare("SELECT name FROM orgs WHERE id IN (SELECT org_id FROM donations WHERE campaign_id = ?)").all(id.id),
-        moneys: db.prepare("SELECT amount_usd FROM donations WHERE campaign_id = ?").all(id.id)
+    const campaignid = params.slug;
+    const campaign = db.prepare("SELECT title, desc, money_needed, money_donated FROM campaigns WHERE id = ?").get(campaignid);
+    const donationrows = db.prepare("SELECT * FROM donations WHERE campaign_id = ?").all(campaignid)
+
+    var donations : any[]= []
+
+    console.log("DONATION ROWS \n\n\n", donationrows[1])
+
+    // console.log(donationrows)
+
+    for(var x = 0; x < donationrows.length; x++){
+        donations.push(new Donation(donationrows[x].id , '', Organization.fromId(donationrows[x].org_id), Number(campaignid), Contact.fromId(donationrows[x].contact_id), donationrows[x].amount_usd, donationrows[x].time ).toJSON())
     }
+
+    console.log("DONATIONS: \n\n\n", donations[1])
+    console.log({
+        campaign: campaign,
+        donations: donations,
+	})
 	return {
         campaign: campaign,
         donations: donations,
